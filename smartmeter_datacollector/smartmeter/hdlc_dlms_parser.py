1dd91ab5657d1d1090ed41c46a1ad584ec018f0f
TtijasS <tugo.strbenc@gmail.com>
2024-12-24 09:18:13 +0100

    Moved pushlist presets to a pushlist_presets.py file

--- smartmeter_datacollector/smartmeter/hdlc_dlms_parser.py
+++ smartmeter_datacollector/smartmeter/hdlc_dlms_parser.py
@@ -19,6 +19,8 @@ from .cosem import Cosem
 from .meter_data import MeterDataPoint
 from .obis import OBISCode
 
+import smartmeter_datacollector.smartmeter.pushlist_presets as pushlist_presets
+
 LOGGER = logging.getLogger("smartmeter")
 
 
@@ -109,12 +111,10 @@ class HdlcDlmsParser:
                 # message contains no OBIS codes, only values, which is not supported.
                 LOGGER.warning("DLMS message is formatted in unknown structure.")
                 LOGGER.info("Trying forced OBIS method")
-                pushlist_telegram_1 = [[40, bytearray(b'\x00\x08\x19\t\x00\xff'), 2, 0], [1, bytearray(b'\x00\x00*\x00\x00\xff'), 2, 0], [1, bytearray(b'\x00\x00`\x01\x02\xff'), 2, 0], [3, bytearray(b'\x01\x00\x01\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x02\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x03\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x04\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00 \x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x004\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00H\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x1f\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x003\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00G\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x15\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00)\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00=\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x16\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00*\x07\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00>\x07\x00\xff'), 2, 0]]
-                pushlist_telegram_2 = [[40, bytearray(b'\x00\t\x19\t\x00\xff'), 2, 0], [1, bytearray(b'\x00\x00*\x00\x00\xff'), 2, 0], [1, bytearray(b'\x00\x00`\x01\x03\xff'), 2, 0], [70, bytearray(b'\x00\x00`\x03\n\xff'), 2, 0], [1, bytearray(b'\x00\x00`\x0e\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x01\x08\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x01\x08\x01\xff'), 2, 0], [3, bytearray(b'\x01\x00\x01\x08\x02\xff'), 2, 0], [3, bytearray(b'\x01\x00\x02\x08\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x02\x08\x01\xff'), 2, 0], [3, bytearray(b'\x01\x00\x02\x08\x02\xff'), 2, 0], [3, bytearray(b'\x01\x00\x03\x08\x00\xff'), 2, 0], [3, bytearray(b'\x01\x00\x04\x08\x00\xff'), 2, 0]]
-                if len(self._dlms_data.value) == 18:
-                    self._dlms_data.value.insert(0, pushlist_telegram_1)
-                elif len(self._dlms_data.value) == 12:
-                    self._dlms_data.value.insert(0, pushlist_telegram_2)
+                if len(self._dlms_data.value) == 12:
+                    self._dlms_data.value.insert(0, pushlist_presets.telegram_len_12)
+                elif len(self._dlms_data.value) == 18:
+                    self._dlms_data.value.insert(0, pushlist_presets.telegram_len_18)
                 else:
                     LOGGER.error("Parsed list len mismatch. Found len: %i", len(self._dlms_data.value))
                     dlms_objects = []
